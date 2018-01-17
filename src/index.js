4536883a88b512ff4eafe6e812ded8a9a918b59c
Sapin Bajracharya <sbnull@gmail.com>
2018-01-17 21:22:36 +0545

    pass animatedScrollY as prop

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ class ParallaxScrollView extends Component {
 			viewHeight: window.height,
 			viewWidth: window.width
 		}
-		this.scrollY = new Animated.Value(0)
+		this.scrollY = this.props.animatedScrollY || new Animated.Value(0)
 		this._footerComponent = { setNativeProps() {} } // Initial stub
 		this._footerHeight = 0
 	}
